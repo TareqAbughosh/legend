743f000ded07e0765ea99ed713346d530b0dcf3d
tareq.abugoush <tareq.abugoush@ustazi.com>
2023-12-22 17:59:10 +0300

    disclaimer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,41 +1,52 @@
 <footer id="footer" class="footer">
-    <div class="container">
+  <div class="container">
       <div class="row-base row">
-        <div class="col-base text-left-md col-md-4">
-            <a href="#" class="brand">
-              Legend<span class="text-primary"> </span>Brokers
-            </a>
+          <div class="col-base text-left-md col-md-4">
+              <a href="#" class="brand">
+                  Legend<span class="text-primary"> </span>Brokers
+              </a>
           </div>
-        <div class="text-center-md col-base col-md-4">
-            <a href="{{route('riskDisclosure')}}" class="author-link">
-                Risk disclosure
-            </a>
-            <br>
-            <br>
-            Address:
-            <br>
-            Office 301, Building no.5, Halab Street, opposite to the UK Embassy, Abbdon, Amman, Jordan.
+          <div class="text-center-md col-base col-md-4">
+              <a href="{{route('riskDisclosure')}}" class="author-link">
+                  Risk disclosure
+              </a>
+              <br>
+              <br>
+              Address:
+              <br>
+              Office 301, Building no.5, Halab Street, opposite to the UK Embassy, Abbdon, Amman, Jordan.
           </div>
-        <div class="text-right-md col-base col-md-4">
-          © Legend Brokers limited {{date('Y')}}. All Rights Reserved.
-        </div>
+          <div class="text-right-md col-base col-md-4">
+              © Legend Brokers limited {{date('Y')}}. All Rights Reserved.
+          </div>
       </div>
-    </div>
-  </footer>
-
-  <!-- Lines -->
-
-  <div class="page-lines">
-    <div class="container">
-      <div class="col-line col-xs-4">
-        <div class="line"></div>
+      <!-- Additional Text -->
+      <div class="row-base row">
+          <div class="col-base col-md-12">
+              <p>
+                  Low spreads and zero commission depend on the product and account type. Over-the-counter leveraged derivative contracts, including contracts for difference and spot foreign exchange, are complex financial instruments. These instruments come with a high risk of losing money rapidly due to leverage. You should consider whether you understand how these products work, whether you can afford to incur losses and whether you have the appropriate risk appetite. We recommend you seek professional advice before investing.
+              </p>
+              <p>
+                  Legend Brokers Limited is a company incorporated with limited liability under the laws of the Emirate of Ras Al Khaimah, United Arab Emirates (UAE), and the federal laws of the UAE, under registration number IC20130350. The information on this site is not directed at residents of the United Arab Emirates, United States, Belgium, Canada, and Singapore and is not intended for use by any person in any jurisdiction where such use would be contrary to local law or regulation.
+              </p>
+          </div>
       </div>
-      <div class="col-line col-xs-4">
-        <div class="line"></div>
-      </div>
-      <div class="col-line col-xs-4">
-        <div class="line"></div>
-        <div class="line"></div>
-      </div>
-    </div>
   </div>
+</footer>
+
+<!-- Lines -->
+
+<div class="page-lines">
+  <div class="container">
+      <div class="col-line col-xs-4">
+          <div class="line"></div>
+      </div>
+      <div class="col-line col-xs-4">
+          <div class="line"></div>
+      </div>
+      <div class="col-line col-xs-4">
+          <div class="line"></div>
+          <div class="line"></div>
+      </div>
+  </div>
+</div>
