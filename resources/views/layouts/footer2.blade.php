f63dafbb9cf78e6b6908e92620b711a87dc533f6
tareq.abugoush <tareq.abugoush@ustazi.com>
2023-12-22 19:49:24 +0300

    footer fix

--- resources/views/layouts/footer2.blade.php
+++ resources/views/layouts/footer2.blade.php
@@ -24,16 +24,16 @@
                 <br>
             Office 301, Building no.5, Halab Street, opposite to the UK Embassy, Abbdon, Amman, Jordan.
         </div>
+        <div class="row-base row">
+          <div class="col-base col-md-12">
+              <p>
+                  Low spreads and zero commission depend on the product and account type. Over-the-counter leveraged derivative contracts, including contracts for difference and spot foreign exchange, are complex financial instruments. These instruments come with a high risk of losing money rapidly due to leverage. You should consider whether you understand how these products work, whether you can afford to incur losses and whether you have the appropriate risk appetite. We recommend you seek professional advice before investing.
+              </p>
+              <p>
+                  Legend Brokers Limited is a company incorporated with limited liability under the laws of the Emirate of Ras Al Khaimah, United Arab Emirates (UAE), and the federal laws of the UAE, under registration number IC20130350. The information on this site is not directed at residents of the United Arab Emirates, United States, Belgium, Canada, and Singapore and is not intended for use by any person in any jurisdiction where such use would be contrary to local law or regulation.
+              </p>
+          </div>
+      </div>
       </div>
     </div>
-    <div class="row-base row">
-      <div class="col-base col-md-12">
-          <p>
-              Low spreads and zero commission depend on the product and account type. Over-the-counter leveraged derivative contracts, including contracts for difference and spot foreign exchange, are complex financial instruments. These instruments come with a high risk of losing money rapidly due to leverage. You should consider whether you understand how these products work, whether you can afford to incur losses and whether you have the appropriate risk appetite. We recommend you seek professional advice before investing.
-          </p>
-          <p>
-              Legend Brokers Limited is a company incorporated with limited liability under the laws of the Emirate of Ras Al Khaimah, United Arab Emirates (UAE), and the federal laws of the UAE, under registration number IC20130350. The information on this site is not directed at residents of the United Arab Emirates, United States, Belgium, Canada, and Singapore and is not intended for use by any person in any jurisdiction where such use would be contrary to local law or regulation.
-          </p>
-      </div>
-  </div>
   </footer>
